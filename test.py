e25e1c86ed65ce26b9e427b00a095e480cc591ad
Natalie <64873273+DoubleSnapps@users.noreply.github.com>
2025-02-14 10:01:09 -0600

    some things

---
+++ test.py
@@ -0,0 +1,18 @@
+from adafruit_motorkit import MotorKit
+
+
+kit = MotorKit()
+motor1 = kit.motor1
+motor2 = kit.motor2
+
+def drive(speed): 
+    motor1.throttle(speed)
+    motor2.throttle(speed)
+    
+def stop():
+    motor1.throttle(0)
+    motor2.throttle(0)
+    
+
+    
+    
