2d0cac20f8b183ada19bd033797524cd3bba2a0b
Natalie <64873273+DoubleSnapps@users.noreply.github.com>
2025-02-19 16:24:06 -0600

    motor invertation fix fix

--- drivebase.py
+++ drivebase.py
@@ -7,16 +7,16 @@ class Drivebase:
         self.motorR = self.kit.motor3
 
     def drive(self, speed): 
-        self.motorL.throttle = speed 
-        self.motorR.throttle = -speed
-        
-    def turnL(self, speed):
-        self.motorL.throttle = speed
+        self.motorL.throttle = -speed 
         self.motorR.throttle = speed
         
-    def turnR(self, speed):
+    def turnL(self, speed):
         self.motorL.throttle = -speed
         self.motorR.throttle = -speed
+        
+    def turnR(self, speed):
+        self.motorL.throttle = speed
+        self.motorR.throttle = speed
         
     def stop(self):
         self.motorL.throttle = 0
